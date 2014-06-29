205f7004b7afb697b8cdf8290fbdca0dbebc7a37
Kyle Meyer <kyle@kyleam.com>
2014-06-29 12:12:59 -0400

    Revise commentary

--- snakemake-mode.el
+++ snakemake-mode.el
@@ -25,7 +25,7 @@
 
 ;; Snakemake mode provides support for editing Snakemake [1] files. It
 ;; builds on Python mode to provide fontification, indentation, and
-;; imenu indexing for Snakemake's rule blocks and builtin functions.
+;; imenu indexing for Snakemake's rule blocks.
 ;;
 ;; If Snakemake mode is installed from MELPA, no additional setup is
 ;; required. It will be loaded the first time a file named 'Snakefile'
