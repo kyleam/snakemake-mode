4f64233f1819ed2832b23883916601381e75e1ed
Kyle Meyer <kyle@kyleam.com>
2016-02-07 17:07:24 -0500

    Add 'script' keyword
    This keyword was added in Snakemake 3.5.

--- snakemake-mode.el
+++ snakemake-mode.el
@@ -109,6 +109,7 @@
                  "priority"
                  "resources"
                  "run"
+                 "script"
                  "shadow"
                  "shell"
                  "threads"
