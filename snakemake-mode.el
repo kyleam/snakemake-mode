27c19be6fec7b198f5e41c20c914f34183917ffb
Kyle Meyer <kyle@kyleam.com>
2016-04-18 01:05:39 -0400

    Bump version

--- snakemake-mode.el
+++ snakemake-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Kyle Meyer <kyle@kyleam.com>
 ;; URL: https://github.com/kyleam/snakemake-mode
 ;; Keywords: tools
-;; Version: 0.3.0
+;; Version: 0.4.0
 ;; Package-Requires: ((emacs "24") (cl-lib "0.5") (magit-popup "2.4.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
