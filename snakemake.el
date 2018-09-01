a5f868f79d98424d59c84b17178a9356254a9504
Kyle Meyer <kyle@kyleam.com>
2018-09-01 00:41:30 -0400

    Change 'guix environment' snippet to use --manifest
    'guix environment' accepts a manifest as of 267379f85 (environment:
    Add --manifest option., 2018-02-15).  Declaring a set of dependencies
    via --manifest is more straightforward than doing so with --load, so
    suggest --manifest instead.

--- snakemake.el
+++ snakemake.el
@@ -627,7 +627,7 @@ that starts a shell with the desired environment.  For example,
 to set up an enviroment with Guix, `snakemake-shell-file-name'
 could point to a script that runs
 
-    guix environment --pure -l guix.scm --ad-hoc snakemake
+    guix environment --pure -m manifest.scm --ad-hoc snakemake
 
 The file 'guix-snakemake-environment' in Snakemake mode's source
 directory can be used for this purpose."
